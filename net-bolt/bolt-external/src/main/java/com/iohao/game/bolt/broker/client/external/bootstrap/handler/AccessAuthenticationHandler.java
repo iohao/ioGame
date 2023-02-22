a202d2dde2269b03f915b3e701c1a2998ec0d87b
渔民小镇 <262610965@qq.com>
2023-02-22 19:23:00 +0800

    :whale:

--- net-bolt/bolt-external/src/main/java/com/iohao/game/bolt/broker/client/external/bootstrap/handler/AccessAuthenticationHandler.java
+++ net-bolt/bolt-external/src/main/java/com/iohao/game/bolt/broker/client/external/bootstrap/handler/AccessAuthenticationHandler.java
@@ -18,11 +18,9 @@ package com.iohao.game.bolt.broker.client.external.bootstrap.handler;
 
 import com.iohao.game.action.skeleton.core.exception.ActionErrorEnum;
 import com.iohao.game.bolt.broker.client.external.bootstrap.message.ExternalMessage;
-import com.iohao.game.bolt.broker.client.external.config.AccessAuthenticationHook;
 import com.iohao.game.bolt.broker.client.external.config.ExternalGlobalConfig;
 import com.iohao.game.bolt.broker.client.external.session.UserSession;
 import com.iohao.game.bolt.broker.client.external.session.UserSessions;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -52,18 +50,15 @@ public final class AccessAuthenticationHandler extends SimpleChannelInboundHandl
     }
 
     static boolean reject(ChannelHandlerContext ctx, ExternalMessage message) {
-        AccessAuthenticationHook accessAuthenticationHook = ExternalGlobalConfig.accessAuthenticationHook;
-        boolean reject = accessAuthenticationHook.reject(message.getCmdMerge());
+        boolean reject = ExternalGlobalConfig.accessAuthenticationHook.reject(message.getCmdMerge());
 
         if (reject) {
-            UserSession userSession = UserSessions.me().getUserSession(ctx);
 
             message.setResponseStatus(ActionErrorEnum.cmdInfoErrorCode.getCode());
             message.setValidMsg(ActionErrorEnum.cmdInfoErrorCode.getMsg());
 
             // 响应结果给玩家
-            Channel channel = userSession.getChannel();
-            channel.writeAndFlush(message);
+            ctx.writeAndFlush(message);
 
             return true;
         }
@@ -76,8 +71,7 @@ public final class AccessAuthenticationHandler extends SimpleChannelInboundHandl
         UserSession userSession = UserSessions.me().getUserSession(ctx);
 
         // 是否可以访问业务方法（action），true 表示可以访问该路由对应的业务方法
-        AccessAuthenticationHook accessAuthenticationHook = ExternalGlobalConfig.accessAuthenticationHook;
-        boolean pass = accessAuthenticationHook.pass(userSession, message.getCmdMerge());
+        boolean pass = ExternalGlobalConfig.accessAuthenticationHook.pass(userSession, message.getCmdMerge());
 
         if (pass) {
             return false;
@@ -87,8 +81,7 @@ public final class AccessAuthenticationHandler extends SimpleChannelInboundHandl
         message.setResponseStatus(ActionErrorEnum.verifyIdentity.getCode());
         message.setValidMsg(ActionErrorEnum.verifyIdentity.getMsg());
         // 响应结果给玩家
-        Channel channel = userSession.getChannel();
-        channel.writeAndFlush(message);
+        ctx.writeAndFlush(message);
 
         return true;
     }
