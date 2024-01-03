5912edcb6d7b090a56118a6a42dba264cc0097e4
渔民小镇 <262610965@qq.com>
2024-01-03 13:39:55 +0800

    :whale: 17.1.61

--- common/common-core/src/main/java/com/iohao/game/action/skeleton/IoGameVersion.java
+++ common/common-core/src/main/java/com/iohao/game/action/skeleton/IoGameVersion.java
@@ -29,7 +29,7 @@ public final class IoGameVersion {
     public static String a;
 
     static {
-        String internalVersion = "<version>17.1.60</version>";
+        String internalVersion = "<version>17.1.61</version>";
 
         VERSION = internalVersion
                 .replace("<version>", "")
