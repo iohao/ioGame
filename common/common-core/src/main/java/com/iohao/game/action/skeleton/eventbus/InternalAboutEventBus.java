360e25a550a60d554d4e2d72a876236489a8f85d
渔民小镇 <262610965@qq.com>
2024-01-21 20:06:30 +0800

    :whale:

--- common/common-core/src/main/java/com/iohao/game/action/skeleton/eventbus/InternalAboutEventBus.java
+++ common/common-core/src/main/java/com/iohao/game/action/skeleton/eventbus/InternalAboutEventBus.java
@@ -19,7 +19,7 @@
 package com.iohao.game.action.skeleton.eventbus;
 
 import com.esotericsoftware.reflectasm.MethodAccess;
-import com.iohao.game.common.kit.TraceKit;
+import com.iohao.game.common.kit.trace.TraceKit;
 import com.iohao.game.common.kit.concurrent.executor.ExecutorRegionKit;
 import com.iohao.game.common.kit.concurrent.executor.ThreadExecutor;
 import lombok.experimental.UtilityClass;
