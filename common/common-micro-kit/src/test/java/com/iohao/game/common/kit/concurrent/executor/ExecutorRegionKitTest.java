e5abfdc33324c9fe08d02d06ac933f1fb80b8061
渔民小镇 <262610965@qq.com>
2024-03-28 10:21:45 +0800

    :whale: add ExecutorRegion test

--- common/common-micro-kit/src/test/java/com/iohao/game/common/kit/concurrent/executor/ExecutorRegionKitTest.java
+++ common/common-micro-kit/src/test/java/com/iohao/game/common/kit/concurrent/executor/ExecutorRegionKitTest.java
@@ -2,9 +2,12 @@ package com.iohao.game.common.kit.concurrent.executor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
 
 /**
  * @author 渔民小镇
@@ -12,9 +15,41 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public class ExecutorRegionKitTest {
+
     @After
     public void tearDown() {
         sleep();
+    }
+
+    @Test
+    public void name() {
+        ExecutorRegion executorRegion = ExecutorRegionKit.getExecutorRegion();
+        ExecutorRegion executorRegion2 = ExecutorRegionKit.createExecutorRegion();
+
+        // ExecutorRegion
+        Assert.assertEquals(executorRegion, ExecutorRegionKit.getExecutorRegion());
+        Assert.assertNotEquals(executorRegion, executorRegion2);
+
+        // global single SimpleThreadExecutor
+        Assert.assertEquals(executorRegion.getSimpleThreadExecutor(0),
+                executorRegion.getSimpleThreadExecutor(0));
+        Assert.assertEquals(executorRegion.getSimpleThreadExecutor(0),
+                ExecutorRegionKit.getSimpleThreadExecutor(0));
+
+        Assert.assertEquals(executorRegion2.getSimpleThreadExecutor(0),
+                executorRegion2.getSimpleThreadExecutor(0));
+        Assert.assertEquals(executorRegion2.getSimpleThreadExecutor(0),
+                ExecutorRegionKit.getSimpleThreadExecutor(0));
+
+        // UserThreadExecutor
+        Assert.assertEquals(executorRegion.getUserThreadExecutor(0)
+                , executorRegion.getUserThreadExecutor(0));
+
+        Assert.assertEquals(executorRegion2.getUserThreadExecutor(0)
+                , executorRegion2.getUserThreadExecutor(0));
+
+        Assert.assertNotEquals(executorRegion.getUserThreadExecutor(0)
+                , executorRegion2.getUserThreadExecutor(0));
     }
 
     @Test
@@ -24,19 +59,35 @@ public class ExecutorRegionKitTest {
         ExecutorRegion executorRegion = ExecutorRegionKit.getExecutorRegion();
         ThreadExecutor userThreadExecutor = executorRegion.getUserThreadExecutor(userId);
 
-        userThreadExecutor.execute(() -> {
-            // print 1
-            log.info("userThreadExecutor : 1");
+        extracted(userThreadExecutor);
+    }
+
+    private void extracted(ThreadExecutor threadExecutor) {
+        Amount amount = new Amount();
+
+        int loop = 100_000;
+        for (int i = 0; i < loop; i++) {
+            threadExecutor.execute(amount::inc);
+        }
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        threadExecutor.execute(() -> {
+            log.info("end - amount : {}", amount);
+            countDownLatch.countDown();
         });
 
-        userThreadExecutor.execute(() -> {
-            // print 2
-            log.info("userThreadExecutor : 2");
-        });
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        Assert.assertEquals(amount.num, loop);
+        log.info("threadExecutor : {}", threadExecutor);
     }
 
     @Test
-    public void getUserVirtualThreadExecutor() {
+    public void userVirtualThreadExecutor() {
         long userId = 1;
 
         ExecutorRegion executorRegion = ExecutorRegionKit.getExecutorRegion();
@@ -54,20 +105,11 @@ public class ExecutorRegionKitTest {
     }
 
     @Test
-    public void getSimpleThreadExecutor() {
+    public void simpleThreadExecutor() {
         long userId = 1;
 
         ThreadExecutor simpleThreadExecutor = ExecutorRegionKit.getSimpleThreadExecutor(userId);
-
-        simpleThreadExecutor.execute(() -> {
-            // print 1
-            log.info("simpleThreadExecutor : 1");
-        });
-
-        simpleThreadExecutor.execute(() -> {
-            // print 2
-            log.info("simpleThreadExecutor : 2");
-        });
+        extracted(simpleThreadExecutor);
     }
 
     void sleep() {
@@ -75,6 +117,21 @@ public class ExecutorRegionKitTest {
             TimeUnit.MILLISECONDS.sleep(100);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static final class Amount {
+        int num;
+
+        void inc() {
+            this.num++;
+        }
+
+        @Override
+        public String toString() {
+            return "Amount{" +
+                    "num=" + num +
+                    '}';
         }
     }
 }
