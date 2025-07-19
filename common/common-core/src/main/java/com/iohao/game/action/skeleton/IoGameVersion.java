27297ac483a1b230fff0ca9f0081c303c215ef0c
iohao <262610965@qq.com>
2025-07-19 18:58:59 +0800

    :whale: 21.29

--- common/common-core/src/main/java/com/iohao/game/action/skeleton/IoGameVersion.java
+++ common/common-core/src/main/java/com/iohao/game/action/skeleton/IoGameVersion.java
@@ -26,7 +26,7 @@ public final class IoGameVersion {
     public static final String VERSION;
 
     static {
-        String internalVersion = "<version>21.28</version>";
+        String internalVersion = "<version>21.29</version>";
 
         VERSION = internalVersion
                 .replace("<version>", "")
